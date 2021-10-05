80a2a4aed13e8c49becb809b6334bb7da04741b5
seemotion <christian@seebode.com>
2021-10-05 11:35:59 +0200

    fixed

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/test_clf.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/test_clf.py
@@ -4,20 +4,20 @@ from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
 
 
-# def test_accuracy():
-#
-#     # Load test data
-#     with open("data/test_data.pkl", "rb") as file:
-#         test_data = pickle.load(file)
-#
-#     # Unpack the tuple
-#     X_test, y_test = test_data
-#
-#     # Compute accuracy of classifier
-#     acc = clf.score(X_test, y_test)
-#
-#     # Accuracy should be over 90%
-#     assert acc > 0.9
+def test_accuracy():
+
+    # Load test data
+    with open("data/test_data.pkl", "rb") as file:
+        test_data = pickle.load(file)
+
+    # Unpack the tuple
+    X_test, y_test = test_data
+
+    # Compute accuracy of classifier
+    acc = clf.score(X_test, y_test)
+
+    # Accuracy should be over 90%
+    assert acc > 0.9
 
 
 def test_pipeline_and_scaler():
